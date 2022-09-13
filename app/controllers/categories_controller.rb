5188a93552c404841e8db11993cd533d2966c4da
Olivia McSweeney <mcsweeno@gmail.com>
2022-09-13 12:46:43 +0100

    CRUD actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,33 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def show
+    @categories = Category.find(params[:id])
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :location, :rating, :review)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "categories", to: "categories#index"
+  get "categories:id", to: "categories#show"
+  get "categories/new", to: "categories#new"
+  post "categories", to: "categories#create"
+  get "categories:id/edit", to: "categories#edit"
+  patch "categories/:id", to: "categories#update"
+  delete "categories/:id", to: "categories#destroy"
 end
