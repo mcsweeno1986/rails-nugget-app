31e80151bfae7ac67378af9bf047851c47bbe7ad
Olivia McSweeney <mcsweeno@gmail.com>
2022-12-06 12:55:50 +0000

    added dotenv gem to access .env file for api key to map

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,16 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
+require 'dotenv/load'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
+HOSTNAME = ENV['HOSTNAME']
 
 module RailsNuggetApp
   class Application < Rails::Application
